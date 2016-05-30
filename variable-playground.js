8c46f7c78efe08ea0fc8fc43f774a269431c7883
manytime777 <manytime@777.so-net.jp>
2016-05-30 22:24:26 +0900

    Add variable plyground area

---
+++ variable-playground.js
@@ -0,0 +1,43 @@
+/*
+var person = {
+	name: 'Andrew',
+	age: 21
+};
+*/
+//function updatePersion (obj) {
+	/*
+	obj = {
+		name: 'Andrew',
+		age: 24
+	};
+	*/
+//	obj.age = 24;
+//}
+
+//updatePersion(person);
+//console.log(person);
+
+// Array Example
+/*
+var array = [
+	{name: 'Dog', age:20},
+	{name: 'Cat', age:22}
+];
+
+array.forEach( function (obj) {
+	obj.age = 24;
+	console.log(obj);
+});
+*/
+
+var grades = [15, 88];
+
+function addGrade (gradesArr) {
+	gradesArr.push(55);
+	debugger;
+	//gradesArr = [12, 33, 99];
+}
+
+addGrade(grades);
+console.log(grades);
+
